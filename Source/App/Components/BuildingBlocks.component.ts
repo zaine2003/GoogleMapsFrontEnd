40455c4f39a692ded2d49b49241ecc13315b69db
MIKE-PC\mikep <mike.p.hughes@hotmail.co.uk>
2017-07-08 08:56:34 +0100

    addded region blocks for future methods  and added spinenr and data service to component

--- Source/App/Components/BuildingBlocks.component.ts
+++ Source/App/Components/BuildingBlocks.component.ts
@@ -10,22 +10,51 @@ namespace BuildingBlocksWeb.Components {
         constructor() {
 
             this.bindings = {
-                x: '<',
+                x: '<', // one way!
             };
 
-            this.controller = BuildingBlocksWebController;
+            this.controller = BuildingBlocksWebComponentController;
             this.templateUrl = '';
         }
     }
 
-    class BuildingBlocksWebController implements ng.IController {
+    class BuildingBlocksWebComponentController implements ng.IController {
 
-        static $inject = [];
+        static $inject = ['BuildingBlocksWebService', 'usSpinnerService'];
 
         public x: string;
 
-        constructor() {}
+        constructor(
+            private BuildingBlocksWebService: Services.BuildingBlocksWebService,
+            private usSpinnerService: ISpinnerService) { }
 
+             $onInit = () => {
+                 this.x = '';
+              }
+
+        // -----------------------------------------------------------------------------------
+        // Service Calls
+        // -----------------------------------------------------------------------------------
+
+        // -----------------------------------------------------------------------------------
+        //  Event handlers
+        // -----------------------------------------------------------------------------------
+
+        // -----------------------------------------------------------------------------------
+        //   Page Events
+        // -----------------------------------------------------------------------------------
+
+        // -----------------------------------------------------------------------------------
+        // Private methods
+        // -----------------------------------------------------------------------------------
+
+        private startSpinner = (spinnerName: string) => {
+            this.usSpinnerService.spin(spinnerName);
+        }
+
+        private stopSpinner = (spinnerName: string) => {
+            this.usSpinnerService.stop(spinnerName);
+        }
 
     }
 
