a47f68f63ac578e0d7fe6356f25dee6b862506e2
MIKE-PC\mikep <mike.p.hughes@hotmail.co.uk>
2017-07-08 16:47:17 +0100

    template change

--- Source/App/App.ts
+++ Source/App/App.ts
@@ -2,7 +2,7 @@ namespace BuildingBlocksWeb.Modules {
 
     angular.module('BuildingBlocksDistanceMatrixDemo', ['angularSpinner', 'ngRoute', 'BuildingBlocksDistanceMatrixDemo'])
     .config(['$routeProvider', '$locationProvider', ($routeProvider, $locationProvider) => {
-        $routeProvider.when('/', { template: './App/Home.html' });
+        $routeProvider.when('/', { templateUrl: './App/Home.html' });
 
         $routeProvider.otherwise({ templateUrl: './App/Home.html' });
 
