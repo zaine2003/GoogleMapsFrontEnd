d52c4d38b8282b690c0ce4846964622e51f6382f
MIKE-PC\mikep <mike.p.hughes@hotmail.co.uk>
2017-07-08 10:29:42 +0100

    expanded app module to include configuration for routing

--- Source/App/App.ts
+++ Source/App/App.ts
@@ -1,10 +1,15 @@
 namespace BuildingBlocksWeb.Modules {
 
-    angular.module('BuildingBlocksWebModule', [
-        /*Core Modules*/
-        /*3rd Party Modules*/
-        'angularSpinner',
+    angular.module('BuildingBlocksDistanceMatrixDemo', ['angularSpinner', 'ngRoute', 'BuildingBlocksDistanceMatrixDemo'])
+    .config(['$routeProvider', '$locationProvider', ($routeProvider, $locationProvider) => {
+        $routeProvider.when('/', { template: './App/Home.html' });
+
+        $routeProvider.otherwise({ templateUrl: './App/Home.html' });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false,
+        });
+    },
     ]);
-
-
 }
