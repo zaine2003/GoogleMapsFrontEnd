623cf8f4805b89f9935b380da5ef24c2b494911d
MIKE-PC\mikep <mike.p.hughes@hotmail.co.uk>
2017-07-08 09:15:28 +0100

    added domain objects for standard reponse

---
+++ Source/App/Domain/ResponseObjects/StandardResponse/DistanceMatrixStandardResponse.ts
@@ -0,0 +1,21 @@
+namespace BuildingBlocksWeb.Domains {
+
+    export interface IDistanceMatrixStandardResponse {
+        status: string;
+        originAddresses: Array<string>;
+        destinationAddresses: Array<string>;
+        rows: Array<Row>;
+    }
+
+    export class DistanceMatrixStandardResponse implements IDistanceMatrixStandardResponse {
+        public status: string;
+        public originAddresses: Array<string>;
+        public destinationAddresses: Array<string>;
+        public rows: Array<Row>;
+        constructor() {
+            this.originAddresses = new Array<string>();
+            this.destinationAddresses = new Array<string>();
+            this.rows = new Array<Row>();
+        }
+    }
+}
---
+++ Source/App/Domain/ResponseObjects/StandardResponse/Row.ts
@@ -0,0 +1,10 @@
+namespace BuildingBlocksWeb.Domains {
+
+    export class Row {
+        public elements: Array<Element>;
+
+        constructor() {
+            this.elements = new Array<Element>();
+        }
+    }
+}
